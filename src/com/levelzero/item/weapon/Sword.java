392eee006efd340f7504313175b7081b0620ab2d
zoom26042604 <nathan.ferre@ynov.com>
2026-02-16 09:58:20 +0100

    feat: add the basic Weapons Type class

---
+++ src/com/levelzero/item/weapon/Sword.java
@@ -0,0 +1,37 @@
+package com.levelzero.item.weapon;
+
+// Represents a sword, an offensive weapon dealing physical damage
+public class Sword extends Weapon {
+    
+    // The physical damage dealt by the sword
+    private int damage;
+    
+    // Sword constructor with damage validation
+    public Sword(String name, int damage, int price) {
+        super(name, "Sword", price);
+        
+        if (damage < 0) {
+            throw new IllegalArgumentException("Damage cannot be negative");
+        }
+        
+        this.damage = damage;
+    }
+    
+    // Returns the sword damage
+    @Override
+    public int getDamage() {
+        return damage;
+    }
+    
+    // Swords provide no defence
+    @Override
+    public int getDefence() {
+        return 0;
+    }
+    
+    // Returns the sword statistics
+    @Override
+    public String getStats() {
+        return String.format("%s (Sword) - Damage: %d, Price: %d", name, damage, price);
+    }
+}
