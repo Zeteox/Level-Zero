a1c8db5bfee0efacc24014948c4efcba39cc7f4c
Zeteox <poussindefeu01@gmail.com>
2026-02-16 10:16:44 +0100

    feat(buildings): added merchant add hostel buildings with the Building interface implemented and the building factory.

--- src/com/levelzero/village/building/Hostel.java
+++ src/com/levelzero/village/building/Hostel.java
@@ -1,4 +1,24 @@
 package com.levelzero.village.building;
 
-public class Hostel {
+public class Hostel implements Building {
+    private final int price;
+
+    public Hostel() {
+        this.price = this.getRandomPrice();
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    private int getRandomPrice() {
+        /**
+         * This method generates a random price for the hostel building. The price is a random integer between 10 and 20.
+         */
+        return (int) (Math.random() * 10) + 10;
+    }
+
+//    public void heal(LivingCreature target) {
+//        target.healHp(target.getMaxHp());
+//    }
 }
--- src/com/levelzero/village/building/Merchant.java
+++ src/com/levelzero/village/building/Merchant.java
@@ -1,4 +1,55 @@
 package com.levelzero.village.building;
 
-public class Merchant {
+import java.util.ArrayList;
+
+public class Merchant implements Building {
+    /**
+     * The MerchantBuilding class represents a building where players can buy and sell items such as weapons and potions.
+     */
+
+//    ArrayList<Weapon> weaponStock;
+//    ArrayList<Potion> potionStock;
+
+//    public MerchantBuilding() {
+//        this.weaponStock = new ArrayList<>();
+//        this.potionStock = new ArrayList<>();
+//    }
+
+//    public ArrayList<Weapon> getWeapons() {
+//        return weaponStock;
+//    }
+
+//    public void addWeapon(Weapon weapon) {
+//        this.weaponStock.add(weapon);
+//    }
+
+//    public void removeWeapon(Weapon weapon) {
+//        this.weaponStock.remove(weapon);
+//    }
+
+//    public void showWeapons() {
+//        System.out.println("Weapons available for sale:");
+//        for (Weapon weapon : weaponStock) {
+//            System.out.println(weapon.getName());
+//        }
+//    }
+
+//    public ArrayList<Potion> getPotions() {
+//        return potionStock;
+//    }
+
+//    public void addPotion(Potion potion) {
+//        this.potionStock.add(potion);
+//    }
+
+//    public void removePotion(Potion potion) {
+//        this.potionStock.remove(potion);
+//    }
+
+//    public void showPotions() {
+//        System.out.println("Potions available for sale:");
+//        for (Potion potion : potionStock) {
+//            System.out.println(potion.getName());
+//        }
+//    }
 }
