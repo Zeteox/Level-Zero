a30c03cad4fb6135e7dc8beae5930fdfb4243231
Zeteox <poussindefeu01@gmail.com>
2026-02-16 17:27:15 +0100

    fix(buildings): fixed the commented method and modified the merchant to add stock + building factory give a level to the merchant now.

--- src/com/levelzero/village/building/BuildingFactory.java
+++ src/com/levelzero/village/building/BuildingFactory.java
@@ -1,10 +1,10 @@
 package com.levelzero.village.building;
 
 public class BuildingFactory {
-    static Building createBuilding(BuildingType type, int level) {
+    public static Building createBuilding(BuildingType type, int level) {
         return switch (type) {
             case HOSTEL -> new Hostel();
-            case MERCHANT -> new Merchant();
+            case MERCHANT -> new Merchant(level);
             case MINE -> new Mine(level);
             default -> throw new IllegalArgumentException("Invalid building type");
         };
