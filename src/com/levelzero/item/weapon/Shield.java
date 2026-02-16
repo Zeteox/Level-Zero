37209678bcbeb4002abd2441b8fc9e11fd9a2b2c
zoom26042604 <nathan.ferre@ynov.com>
2026-02-16 09:58:20 +0100

    feat: add the basic Weapons Type class

---
+++ src/com/levelzero/item/weapon/Shield.java
@@ -0,0 +1,37 @@
+package com.levelzero.item.weapon;
+
+// Represents a shield providing defensive protection
+public class Shield extends Weapon {
+    
+    // The defence value provided by the shield
+    private int defence;
+    
+    // Shield constructor with defence validation
+    public Shield(String name, int defence, int price) {
+        super(name, "Shield", price);
+        
+        if (defence < 0) {
+            throw new IllegalArgumentException("Defence cannot be negative");
+        }
+        
+        this.defence = defence;
+    }
+    
+    // Shields deal no damage
+    @Override
+    public int getDamage() {
+        return 0;
+    }
+    
+    // Returns the defence provided by the shield
+    @Override
+    public int getDefence() {
+        return defence;
+    }
+    
+    // Returns the shield statistics
+    @Override
+    public String getStats() {
+        return String.format("%s (Shield) - Defence: %d, Price: %d", name, defence, price);
+    }
+}
---
+++ src/com/levelzero/item/weapon/Staff.java
@@ -0,0 +1,37 @@
+package com.levelzero.item.weapon;
+
+// Represents a magic staff dealing magic damage
+public class Staff extends Weapon {
+    
+    // The magic damage dealt by the staff
+    private int damage;
+    
+    // Staff constructor with damage validation
+    public Staff(String name, int damage, int price) {
+        super(name, "Staff", price);
+        
+        if (damage < 0) {
+            throw new IllegalArgumentException("Damage cannot be negative");
+        }
+        
+        this.damage = damage;
+    }
+    
+    // Returns the staff magic damage
+    @Override
+    public int getDamage() {
+        return damage;
+    }
+    
+    // Staves provide no defence
+    @Override
+    public int getDefence() {
+        return 0;
+    }
+    
+    // Returns the staff statistics
+    @Override
+    public String getStats() {
+        return String.format("%s (Staff) - Magic Damage: %d, Price: %d", name, damage, price);
+    }
+}
