f48af64b05f637eeec310cdae490a66b91da1b1f
Loïc <loic.delprat@ynov.com>
2026-02-17 23:55:22 +0100

    documentation(mixed): done the documentation of all buildings except mine and the village + trade utils + modification the the readme project structure.

--- src/com/levelzero/village/building/Hostel.java
+++ src/com/levelzero/village/building/Hostel.java
@@ -2,24 +2,39 @@ package com.levelzero.village.building;
 
 import com.levelzero.creature.LivingCreature;
 
+/**
+ * The Hostel class represents a building in the village that can heal. It implements the Building interface and has a price associated with it.
+ */
 public class Hostel implements Building {
     private final int price;
 
+    /**
+     * Constructor for the Hostel class. It initializes the price of the hostel with a random method.
+     */
     public Hostel() {
         this.price = this.getRandomPrice();
     }
 
+    /**
+     * Getter method for the price of the hostel.
+     * @return the price of the hostel
+     */
     public int getPrice() {
         return price;
     }
 
+    /**
+     * Method to get the random price.
+     * @return the price of the hostel, which is a random integer between 10 and 20
+     */
     private int getRandomPrice() {
-        /**
-         * This method generates a random price for the hostel building. The price is a random integer between 10 and 20.
-         */
         return (int) (Math.random() * 10) + 10;
     }
 
+    /**
+     * Method to heal a living creature. It heals the target creature to its maximum HP.
+     * @param target the living creature to be healed
+     */
     public void heal(LivingCreature target) {
         target.healHp(target.getMaxHp());
     }
