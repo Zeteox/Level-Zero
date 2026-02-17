31c29ae70b87ad4217c646043b5fa2b2d1d126b2
Zeteox <poussindefeu01@gmail.com>
2026-02-17 11:00:28 +0100

    feat(Utils): added an utility singleton for the merchant - hero trade.

---
+++ src/com/levelzero/utils/TradeUtils.java
@@ -0,0 +1,62 @@
+package com.levelzero.utils;
+
+import com.levelzero.creature.hero.Hero;
+import com.levelzero.item.potion.Potion;
+import com.levelzero.item.weapon.Weapon;
+import com.levelzero.village.building.Merchant;
+
+public class TradeUtils {
+    private static TradeUtils instance;
+
+    private TradeUtils() {}
+
+    public static TradeUtils getInstance() {
+        if (instance == null) {
+            instance = new TradeUtils();
+        }
+        return instance;
+    }
+
+    public void buyItem(Hero hero, Merchant merchant, Weapon weapon) {
+        /**
+         * Implement the logic to buy a weapon from the merchant
+         */
+        if (hero.getGold() - weapon.getPrice() < 0) {
+            System.out.println("You don't have enough money to buy that!");
+            return;
+        }
+        merchant.removeWeapon(weapon);
+        hero.getInventory().addWeapon(weapon);
+        hero.setGold(hero.getGold() - weapon.getPrice());
+    }
+
+    public void buyItem(Hero hero, Merchant merchant, Potion potion) {
+        /**
+         * Implement the logic to buy a potion from the merchant
+         */
+        if (hero.getGold() - potion.getPrice() < 0) {
+            System.out.println("You don't have enough money to buy that!");
+            return;
+        }
+        merchant.removePotion(potion);
+        hero.getInventory().addPotion(potion);
+        hero.setGold(hero.getGold() - potion.getPrice());
+    }
+
+    public void sellItem(Hero hero, Merchant merchant, Weapon weapon) {
+        /**
+         * Implement the logic to sell a weapon to the merchant
+         */
+        hero.getInventory().removeWeapon(weapon);
+        merchant.addWeapon(weapon);
+        hero.setGold(hero.getGold() + weapon.getPrice());
+    }
+    public void sellItem(Hero hero, Merchant merchant, Potion potion) {
+        /**
+         * Implement the logic to sell a potion to the merchant
+         */
+        hero.getInventory().removePotion(potion);
+        merchant.addPotion(potion);
+        hero.setGold(hero.getGold() + potion.getPrice());
+    }
+}
