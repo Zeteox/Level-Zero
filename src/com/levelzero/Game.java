9c01a5d972d3f14a1d90661107b6d1e06dcde0ab
Oiha_dev <cenes.gauthier@gmail.com>
2026-02-18 13:52:27 +0100

    feat(Game): add sword and shield creation and equip them to the hero

--- src/com/levelzero/Game.java
+++ src/com/levelzero/Game.java
@@ -3,7 +3,11 @@ package com.levelzero;
 import com.levelzero.creature.hero.Hero;
 import com.levelzero.creature.hero.HeroFactory;
 import com.levelzero.creature.hero.HeroType;
+import com.levelzero.item.weapon.Shield;
+import com.levelzero.item.weapon.Sword;
 import com.levelzero.village.Village;
+
+import static com.levelzero.item.weapon.WeaponFactory.*;
 
 public class Game {
     private Hero hero;
@@ -20,7 +24,14 @@ public class Game {
             throw new IllegalArgumentException("Hero type cannot be null");
         }
 
+        Sword sword = createSword("Basic Sword", 10, 100);
+        Shield shield = createShield("Basic Shield", 5, 50);
+
         this.hero = createHero(heroName, heroType);
+        hero.getInventory().addItem(sword);
+        hero.getInventory().addItem(shield);
+        hero.equip(sword);
+        hero.equip(shield);
         this.village = createVillage("Village", 1);
     }
 
